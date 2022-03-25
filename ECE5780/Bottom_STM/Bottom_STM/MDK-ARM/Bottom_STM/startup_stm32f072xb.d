9f20660743755e65131db5b308c3d9d3104984fb
JohnMismash <u1179865@utah.edu>
2022-03-25 11:11:26 -0600

    Started both projects with Keil.

---
+++ ECE5780/Bottom_STM/Bottom_STM/MDK-ARM/Bottom_STM/startup_stm32f072xb.d
@@ -0,0 +1,1 @@
+bottom_stm\startup_stm32f072xb.o: startup_stm32f072xb.s
---
+++ ECE5780/Bottom_STM/Bottom_STM/MDK-ARM/RTE/_Bottom_STM/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Bottom_STM' 
+ * Target:  'Bottom_STM' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
